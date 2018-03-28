7720715fc6a2e1a4965bc57cc81efa9e178a6ab9
A. Oyshi <37639117+aoyshi@users.noreply.github.com>
2018-03-27 23:57:55 -0500

    .java file
    trimmed out redundant program description

--- BulgarianSolitaire.java
+++ BulgarianSolitaire.java
@@ -1,16 +1,4 @@
-/**
- * This program models the game of Bulgarian Solitaire. The game 
- * starts with 45 cards. This deck is randomly divided into some number of piles of random 
- * size. For example, you might start with piles of size 20, 5, 1, 9, and 10. In 
- * each round, you take one card from each pile, forming a new pile with these cards. 
- * For example, the sample starting configuration would be transformed into piles of 
- * size 19, 4, 8, 9, and 5. The solitaire is over when the piles have size 
- * 1, 2, 3, 4, 5, 6, 7, 8, and 9, in some order. (It can be shown that you always 
- * end up with such a configuration). This program produces a random starting 
- * configuration and prints it. Then it keeps applying the solitaire step and prints the resulting decks, 
- * stopping when the solitaire final configuration of 1,2,3,4,5,6,7,8,9 is reached. 
-*/
-package Ch6A_Arrays;
+/*Bulgarian Solitaire model*/
 
 public class BulgarianSolitaire {
   
